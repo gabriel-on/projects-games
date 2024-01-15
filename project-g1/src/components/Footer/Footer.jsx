275c612feb2a3ba8fcbc7040bb1af9cd16753fd3
Gabriel ON <gabrielblackbr1@gmail.com>
2024-01-15 10:09:41 -0300

    update - deploy - version 1.2

--- project-g1/src/components/Footer/Footer.jsx
+++ project-g1/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer>
       <div>
-        <p>&copy; - Pre_Alpha - Version: 1</p>
+        <p>&copy; - Pre_Alpha - Version: 1.2</p>
       </div>
     </footer>
   )
